726fb10301a66419cf1efce5be138fbdc86fb84f
eIdino <x00153243@mytudublin.ie>
2021-12-10 13:44:15 +0000

    added document and removed 2 pages

---
+++ CryptoService/InputValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using CryptoService.Pages;
+
+namespace CryptoService
+{
+    // Custom validation class
+    public class CheckPairNameValidationAttribute : ValidationAttribute
+    {
+        // Custom validation function
+        protected override ValidationResult IsValid(object value,
+        ValidationContext validationContext)
+        {
+            // Converts value into a string
+            string valueString = Convert.ToString(value);
+            {
+                // Checks if the length of the value is in between 5 and 12 and contains letters only
+                if (valueString.Length > 5 && valueString.Length < 12 && valueString.All(char.IsLetter))
+                {
+                        return ValidationResult.Success;
+                }
+            }
+            // if the value does not pass above, sends error message
+            return new ValidationResult("Enter Valid Pair Name from the Pair List",
+                new[] { validationContext.MemberName });
+        }
+    }
+}
---
+++ CryptoService/Model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace CryptoService
+{
+    public class Model
+    {
+        // This field must have a value
+        [Required]
+        // Calls custom validation
+        [CheckPairNameValidation]
+        public String PairName { get; set; }
+
+        [Required]
+        // Checks that the value is a int and only in the range of 1 - 24
+        [Range(typeof(int), "1", "24", 
+        ErrorMessage = "Enter valid amount of Hours")]
+        public int Hours { get; set; }
+
+        [Required]
+        // Checks that the value is int and only in the range of 1 - 10
+        [Range(typeof(int), "1", "10",
+        ErrorMessage = "Enter valid amount of Data Points (1-10)")]
+        public int DataPoints { get; set; }
+    }
+}
